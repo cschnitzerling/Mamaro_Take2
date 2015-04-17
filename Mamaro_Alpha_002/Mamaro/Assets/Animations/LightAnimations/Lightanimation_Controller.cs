806d08ad9d7a68c3472c171b4e4320aaba6bd319
Eugene <jhonnywhiplash@hotmail.com.au>
2015-04-18 01:21:23 +1000

    -Comic Scene Started- 3 Parts Already finished Signal from Space panel World Populated Panel Destruction Panel

---
+++ Mamaro_Alpha_002/Mamaro/Assets/Animations/LightAnimations/Lightanimation_Controller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightanimation_Controller : MonoBehaviour {
+	public Animator anim;
+	// Use this for initialization
+	void Awake () {
+		anim = GetComponent<Animator> ();
+		anim.SetBool ("Active", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("space")) {
+			anim.SetBool ("Active", true);
+		}
+	}
+}
