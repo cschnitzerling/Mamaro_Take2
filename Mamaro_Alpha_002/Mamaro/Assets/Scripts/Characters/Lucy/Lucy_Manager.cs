7e2a1f0a305665200ce4a88f2e8e8fd642d0f429
JimVincent <1000343@student.sae.edu.au>
2015-04-17 16:14:40 +1000

    Updated Malfunction code

---
+++ Mamaro_Alpha_002/Mamaro/Assets/Scripts/Characters/Lucy/Lucy_Manager.cs
@@ -0,0 +1,295 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lucy_Manager : MonoBehaviour {
+
+	public enum LucyState{Idle, Repair, Scared, Petrified, Tapping};
+	public static Lucy_Manager inst;
+
+	[SerializeField]
+	private LucyState state;
+	Mamaro_Manager mamaro;
+	GameObject lucyTapping;
+
+	[System.NonSerialized]
+	public int fear;
+	public int fearMax;
+	public int fearScaredLevel;
+	public int fearFrightenedLevel;
+
+	public float fearDecreaseTime;
+	private float timerFearDecrease;
+
+	public float repairDelay;//The time after takeing fear damage untill recharge begins.
+	private float timerRepairDelay;//timer for the delay on recharge
+
+	public float repairInterval;//The timer in between when lucy is scared and repairing.
+	private float timerRepair;
+
+	public int repairAmountRepair;//The Amount that lucy repairs mamaro
+	private int repairAmountScared;//The Amount that lucy repairs mamar While in scared mode
+
+	private bool isRepairing;
+
+	private Vector3 meterVec = Vector3.one;
+	public RectTransform meter;
+	public Material color;
+
+	void Awake()
+	{
+		if (inst == null)
+		{
+			inst = this;
+		}
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		fear = 50;
+		mamaro = Mamaro_Manager.inst;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		meterVec.y = ((float)fear / (float)fearMax);
+		meter.localScale = meterVec;
+		if (fear > 0)
+		{
+			timerFearDecrease += Time.deltaTime;
+			if (timerFearDecrease > fearDecreaseTime)
+			{
+				OnChangeFear(FearType.Decrease);
+				timerFearDecrease = 0;
+			}
+				if (fear < 0)
+			{
+				fear = 0;
+			}
+		}
+
+		//Check to see if mamaro is in malfuntioning quicktime event.
+		if (mamaro.isMalfunctioning)
+		{
+			ChangeState(LucyState.Tapping);
+		}
+
+		//Switch for each of Lucy's Staes.
+		switch (state)
+		{
+		case LucyState.Idle:
+			Idle ();
+			break;
+		case LucyState.Repair:
+			Repair();
+			break;
+		case LucyState.Scared:
+			Scared();
+			break;
+		case LucyState.Petrified:
+			Petrified ();
+			break;
+		case LucyState.Tapping:
+			Tapping ();
+			break;
+		}
+
+		///////////////////////////////////////////////////
+		/// Input Hacks                                 //
+		///                                            //
+		if (Input.GetKeyDown(KeyCode.P))              //
+		{                                            //
+			OnChangeFear(FearType.Damage);          //
+		}                                          //
+		if (Input.GetKeyDown(KeyCode.O))          //
+		{                                        //
+			mamaro.health -= 20;                //
+		}                                      //
+		////////////////////////////////////////
+
+
+
+
+
+	}
+
+	//Allows the Fear Meter to be increased or decreased.
+	public void OnChangeFear(FearType fearType)
+	{
+		if (fear <= fearMax && fear >= 0)
+		{
+			fear += (int)fearType;
+		}
+		if (fear > fearMax)
+		{
+			fear = fearMax;
+		}
+		else if (fear < 0)
+		{
+			fear = 0;
+		}
+
+
+
+		if (fear > fearFrightenedLevel)
+		{
+
+			ChangeState(LucyState.Petrified);
+		}
+		else if(fear > fearScaredLevel)
+		{
+			ChangeState(LucyState.Scared);
+		}
+		else 
+		{
+			ChangeState(LucyState.Idle);
+		}
+	}
+
+	/// <summary>
+	/// Lucy's Idle State
+	/// </summary>
+	// Allows Lucy to Run Idle Animation
+	// Change to Repair if Mamaro looses health
+	// Play Lucy Audio
+	void Idle()
+	{
+		if (mamaro.health < mamaro.maxHealth)
+		{
+			ChangeState(LucyState.Repair);
+		}
+	}
+
+	/// <summary>
+	/// Lucy's Repair State
+	/// </summary>
+	/// Plays Repair Animation
+	/// Slowly Repairs Mamaro
+	void Repair()
+	{
+		if (mamaro.health == mamaro.maxHealth)
+		{
+			ChangeState(LucyState.Idle);
+		}
+
+		RepairMamaro(repairAmountRepair);
+
+	}
+
+	/// <summary>
+	/// Scared this instance.
+	/// </summary>
+	/// Repairs Mamaro Intermitently
+	/// Swaps between repai animaion and Scared Animation.
+	void Scared()
+	{
+		RepairMamaro(repairAmountScared);
+	}
+
+	/// <summary>
+	/// Lucy's Petrified State
+	/// </summary>
+	/// Plays Petrified Animation
+	/// will not repair mamaro
+	void Petrified()
+	{
+
+	}
+
+	/// <summary>
+	/// For Use when entering Maamaros Quick time event
+	/// </summary>
+	/// Activates Lucy's Tapping Gameobject on mamaros eye lens
+	void Tapping()
+	{
+		if (!mamaro.isMalfunctioning)
+		{
+			ChangeState(LucyState.Idle);
+		}
+	}
+
+	/// <summary>
+	/// Changes the state.
+	/// </summary>
+	/// <param name="lState">lState.</param>
+	void ChangeState(LucyState lState)
+	{
+		switch (lState)
+		{
+		case LucyState.Idle:
+			color.color = Color.blue;
+			break;
+		case LucyState.Repair:
+			color.color = Color.blue;
+			break;
+		case LucyState.Scared:
+			color.color = Color.yellow;
+			break;
+		case LucyState.Petrified:
+			color.color = Color.red;
+			break;
+		case LucyState.Tapping:
+			break;
+		}
+		state = lState;
+	}
+
+
+	/// <summary>
+	/// Modifies mamaros health
+	/// </summary>
+	/// <param name="amount">Amount.</param>
+	void RepairMamaro(int amount)
+	{
+		timerRepair += Time.deltaTime;
+		
+		//Reparing
+		if (timerRepair > repairInterval)
+		{
+			if (mamaro.health < mamaro.maxHealth)
+			{
+				mamaro.health += amount;
+				timerRepair -= repairInterval;
+			}
+
+			if (mamaro.health > mamaro.maxHealth)
+			{
+				mamaro.health = mamaro.maxHealth;
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Mamaro_Alpha_002/Mamaro/Assets/Scripts/Characters/Mamaro/Mamaro_Manager.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mamaro_Manager : MonoBehaviour 
+{
+	// static instance
+	public static Mamaro_Manager inst;
+
+	// static class access
+	private Game_Manager GM;
+	private Ability_Manager abMan;
+	private Cam_Manager cam;
+	private Lucy_Manager lucy;
+
+	// inspector assigned vars
+	public int maxHealth = 100;
+	public int smallDamage, mediumDamage, largeDamage;	// in regards to how much cam shake to apply
+	public Vector3 camMalfuncPos;
+
+	// non-inspector assigned vars
+	//[HideInInspector]
+	public int health;
+	public bool isMalfunctioning = false;
+	public bool isBlocking = false;
+
+	// private vars
+	public int meleeCores, speedCores, rangedCores, shieldCores;
+
+	void Awake()
+	{
+		if (inst == null)
+			inst = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		GM = Game_Manager.inst;
+		abMan = Ability_Manager.inst;
+		cam = Cam_Manager.inst;
+		health = maxHealth;
+		lucy = Lucy_Manager.inst;
+
+		//To RemoveWArning ############################################################
+		int i = (int)lucy.fear;													  //###
+		i ++;																	  //###
+		// ############################################################################
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// update local ability vars from abMan
+		UpdateCores();
+
+		////////////////////////////////////////////
+		/// Testing damage function	///////////////
+		//////////////////////////////////////////
+		if(Input.GetKeyDown(KeyCode.F4))	/////
+		{								   /////
+			//int testDamage = largeDamage; /////
+			//OnTakeDamage(testDamage);
+			OnCorePickUp();
+		}							    /////
+		////////////////////////////////////
+	}
+
+	// applies damage of x amount
+	public void OnTakeDamage(int amount)
+	{
+		// check if any health
+		if(health > 0)
+		{
+			// let Lucy know
+			//lucy.OnChangeFear(FearType.Damage);
+
+			// check for dead
+			if(health - amount <= 0)
+			{
+				//TODO commence death protocol
+				//TODO play death audio
+			}
+			else
+			{
+				health -= amount;
+
+				// apply appropriate cam shake
+				if(amount >= largeDamage)
+				{
+					cam.ShakeCam(Shake.Large);
+					//TODO play large damage audio
+				}
+				else if(amount <= smallDamage)
+				{
+					cam.ShakeCam(Shake.Small);
+					//TODO play small damage audio
+				}
+				else
+				{
+					cam.ShakeCam(Shake.Medium);
+					//TODO play medium damage audio
+				}
+			}
+		}
+	}
+
+	/// triggers malfunction sequence
+	public void OnCorePickUp()
+	{
+		abMan.AddSpareCore();
+		isMalfunctioning = true;
+		GM.MalfunctionMode(true);
+	}
+
+	// adds x amount to health
+	public void OnRepair(int amount)
+	{
+		// check if health is full
+		if(health < maxHealth)
+		{
+			// don't overfill health
+			if(health + amount > maxHealth)
+				health = maxHealth;
+			else
+				health += amount;
+
+			//TODO play fixing audio here
+		}
+	}
+
+	/// returns the total amount of cores aquired
+	public int GetTotalCores()
+	{
+		return meleeCores + rangedCores + shieldCores + speedCores + abMan.GetSpareCount();
+	}
+
+	// links core values with Ability_Manager
+	private void UpdateCores()
+	{
+		meleeCores = abMan.sockets[0].GetCoreCount();
+		speedCores = abMan.sockets[1].GetCoreCount();
+		rangedCores = abMan.sockets[2].GetCoreCount();
+		shieldCores = abMan.sockets[3].GetCoreCount();
+	}
+}
