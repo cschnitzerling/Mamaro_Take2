105c3c28ac806a967d6ab67375e708bb15d446f8
Eugene <jhonnywhiplash@hotmail.com.au>
2015-04-20 01:46:32 +1000

    Finished Up comic Scene, camara Motion added, just needs lerp and start/end bits and good to go

---
+++ Mamaro_Alpha_002/Mamaro/Assets/Scripts/Comic_Scripts/Comic_Camera_Move.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Comic_Camera_Move : MonoBehaviour {
+
+	public List<Transform> targets = new List<Transform>();
+	public Transform target, curPos;
+	public int counter;
+	// Use this for initialization
+	void Awake () {
+		counter = 1;
+		AddTargets ();
+	}
+
+	public void AddTargets()
+	{
+		GameObject[] targo = GameObject.FindGameObjectsWithTag ("Targets");
+		
+		foreach (GameObject targ in targo)
+			AddTarget (targ.transform);
+		
+	}
+
+	public void AddTarget(Transform target)
+	{
+		targets.Add (target);
+	}
+
+	public void SortedList(){
+		targets = targets.OrderBy(target => target.gameObject.name).ToList();
+		}
+
+	public void NewTarget()
+	{
+		if (target == null) {
+			//SortTargetByDistance ();
+			target = targets [0];
+		} else {
+			int Index = targets.IndexOf(target);
+			if(Index < targets.Count - 1)
+			{
+				Index ++;
+			}else{
+				Index = 0;
+			}
+			//DeselectTarget();
+			target = targets[Index];
+		}
+		//SelectTarget();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		SortedList ();
+		curPos = gameObject.transform;
+		if (Input.GetKeyDown ("1")) {
+			NewTarget();
+		}
+		if (target != null) {
+			transform.position = Vector3.Lerp (curPos.position, target.position, 1);
+		}
+	}
+}
