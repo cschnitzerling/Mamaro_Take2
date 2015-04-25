2e28af03aa9b0d156688612e7bb93658128ee834
JimVincent <1000343@student.sae.edu.au>
2015-04-25 15:43:40 +1000

    Added health when increased

--- Mamaro_Alpha_002/Mamaro/Assets/Mamaro_Health.cs
+++ Mamaro_Alpha_002/Mamaro/Assets/Mamaro_Health.cs
@@ -6,11 +6,15 @@ public class Mamaro_Health : MonoBehaviour {
 	private Vector3 meterVec = Vector3.one;
 	public RectTransform meter;
 	public Material color;
+	public GameObject healthUpParticles;
+
+	private int previousHealth;	// for checking if the health has gone up or down
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		// set pHealth to starting val
+		previousHealth = 100;
 	}
 	
 	// Update is called once per frame
@@ -31,5 +35,26 @@ public class Mamaro_Health : MonoBehaviour {
 		{
 			color.color = Color.yellow;
 		}
+
+		// check for health change
+
+		// particle effect at the relative end of health bar
+		float effectPos = meter.rect.height * Mamaro_Manager.inst.health / Mamaro_Manager.inst.maxHealth;
+
+		// health has increased
+		if(previousHealth < Mamaro_Manager.inst.health)
+		{
+			// show health increase particles
+			healthUpParticles.transform.localPosition = new Vector3(0, effectPos, 0);
+			healthUpParticles.SetActive(true);
+		}
+		// has decreased
+		else if(previousHealth < Mamaro_Manager.inst.health)
+		{
+			//TODO optional: show decrease effect
+		}
+
+		// update pHealth to current
+		previousHealth = Mamaro_Manager.inst.health;
 	}
 }
