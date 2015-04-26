ccaeb28553e623fbdaefe1ce663260aa0c1fe45c
Eugene <jhonnywhiplash@hotmail.com.au>
2015-04-27 04:51:13 +1000

    Lucy Close Up imported Win State Started Level Messed around a bit City nearly done Nav mesh rebaked

---
+++ Mamaro_Alpha_002/Mamaro/Assets/Scripts/WinScene/Mamaro_WinScene.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mamaro_WinScene : MonoBehaviour {
+
+	public Animator anim;
+	public bool handAnim;
+	public bool armActive;
+	public bool endScene = false, walkOff = false;
+	public float time;
+	public FadeInOut fadeOut;
+	// Use this for initialization
+	void Awake () {
+		//fadeOut = GameObject.FindGameObjectWithTag ("FadeScene").GetComponent<FadeInOut> ();
+		anim = GetComponent<Animator> ();
+		anim.SetBool ("Active", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("ArmActive", armActive);
+		transform.Translate(0,0,.06f);
+		if (walkOff) {
+			//transform.Translate(0,0,.06f);
+		}
+		if (endScene) {
+			//fadeOut.nextSceneTwo = true;
+		}
+		}
+
+	public void ArmActive(){
+		anim.SetBool ("ArmActive", true);
+	}
+}
