8521b78be8020113d624fa31b5c8bbf4d218a4da
CSchnitzerling <chris.schnitzerling@hotmail.com>
2015-04-26 15:29:27 +1000

    New Camera Movment

---
+++ Mamaro_Alpha_002/Mamaro/Assets/Scripts/Comic_Scripts/Comic_Controller_2.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Comic_Controller_2 : MonoBehaviour {
+
+	public List<Comic_Target> targets;
+
+	int curTarget;
+
+	public Color fadeColor;
+
+	float timerPause;
+	float timerMove;
+	float timerFade;
+
+	public float fadeTime;
+
+	bool isMove;
+
+	bool isEnd;
+
+	public string nextScene;
+
+	public Material fadeMaterial;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		isMove = false;
+		timerPause = targets[curTarget].pauseTime;
+		transform.position =  targets[curTarget].GetPosition();
+		transform.rotation =  targets[curTarget].GetRotation();
+
+		fadeColor.a = 0;
+		fadeMaterial.color = fadeColor;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isEnd)
+		{
+			timerFade += Time.deltaTime / fadeTime;
+
+			fadeColor.a = timerFade;
+			fadeMaterial.color = fadeColor;
+
+			if (timerFade > 1)
+			{
+				//Application.LoadLevel(nextScene);
+				print("Finish");
+			}
+		}
+		else
+		{
+			if (isMove)
+			{
+				timerMove += Time.deltaTime / targets[curTarget].moveTime;
+
+				transform.position = Vector3.Lerp(targets[curTarget - 1].GetPosition(), targets[curTarget].GetPosition(), timerMove);
+				transform.rotation = Quaternion.Lerp(targets[curTarget - 1].GetRotation(), targets[curTarget].GetRotation(), timerMove);
+
+				if (timerMove > 1)
+				{
+					transform.position =  targets[curTarget].GetPosition();
+					transform.rotation =  targets[curTarget].GetRotation();
+					timerMove = 0;
+					isMove = false;
+				}
+
+			}
+			else
+			{
+				timerPause -= Time.deltaTime;
+				if (timerPause <= 0)
+				{
+					NextTarget();
+					isMove = true;
+				}
+		}
+		}
+	}
+
+	public void NextTarget()
+	{
+		curTarget ++;
+
+		if (curTarget == targets.Count)
+		{
+			OnEndComic();
+		}
+		else
+		{
+			timerPause = targets[curTarget].pauseTime;
+		}
+
+	}
+
+	public void OnEndComic()
+	{
+		isEnd = true;
+	}
+}
---
+++ Mamaro_Alpha_002/Mamaro/Assets/Scripts/Comic_Scripts/Comic_Target.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Comic_Target : MonoBehaviour {
+
+	Vector3 pos;
+	Quaternion rot;
+
+	public float pauseTime;
+	public float moveTime;
+
+	// Use this for initialization
+	void Start () 
+	{
+		pos = gameObject.transform.position;
+		rot = gameObject.transform.rotation;
+	}
+
+	public Vector3 GetPosition()
+	{
+		return pos;
+	}
+
+	public Quaternion GetRotation()
+	{
+		return rot;
+	}
+
+}
