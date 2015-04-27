3ce81144537e34cd71e62a582d0640b3e18ae081
CSchnitzerling <chris.schnitzerling@hotmail.com>
2015-04-27 12:58:35 +1000

    Animation Audio

---
+++ Mamaro_Alpha_002/Mamaro/Assets/AnimatorAudio.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorAudio : MonoBehaviour {
+
+	public void PlayAudio()
+	{
+		Audio_Manager.inst.PlayOnce(AA.Chr_Mamaro_Movement_Standalone_Footstep_2,Camera.main.transform.position);
+	}
+}
